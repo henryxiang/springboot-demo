dd9d337838dee8932f52f9f39a2cae403fcf62ca
Henry Xiang <henryxiang@gmail.com>
2016-03-09 20:37:46 -0800

    Added Spring Boot DevTools dependency.

--- src/main/java/com/example/controller/WelcomeController.java
+++ src/main/java/com/example/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 	@RequestMapping(value="/hello")
 	public String hello() {
-		return "Hello World!";
+		return "Hello Spring!";
+	}
+	
+	@RequestMapping(value="/user/{name}")
+	public String user(@PathVariable String name) {
+		return "Welcome " + name;
 	}
 }
