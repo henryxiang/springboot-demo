90becfc4f8a7114aed8d2a746fe7d4f6f13b9c09
Henry Xiang <hxxiang@gmail.com>
2016-03-10 19:57:19 -0800

    Created logback.xml and use Lombok @Slf4j as logger.

--- src/main/java/com/example/controller/WelcomeController.java
+++ src/main/java/com/example/controller/WelcomeController.java
@@ -4,15 +4,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class WelcomeController {
 	@RequestMapping(value="/hello")
 	public String hello() {
-		return "Hello Spring!";
+		return "Hello Spring Boot!";
 	}
 	
 	@RequestMapping(value="/user/{name}")
 	public String user(@PathVariable String name) {
+		log.info(String.format("name = %s", name));
 		return "Welcome " + name;
 	}
 }
