06c0e957664fe1c9d31759095b5ebb288517868a
Henry Xiang <hxxiang@gmail.com>
2016-03-11 21:12:15 -0800

    modified server.js to add a mock API

--- src/main/javascript/server.js
+++ src/main/javascript/server.js
@@ -1,10 +1,23 @@
 var express = require('express'),
-  server = express(),
-  port = '8000',
-  documentRoot = __dirname + '/build';
+    faker = require('faker'),
+    moment = require('moment'),
+    server = express(),
+    port = '8000',
+    documentRoot = __dirname + '/build';
 
 console.log("Use document root: " + documentRoot);
 server.use("/", express.static(documentRoot));
+
+server.get("/user/:id", function(req, res) {
+  var user = {
+    id: req.params.id,
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    birthday: faker.date.past()
+  };
+  res.json(user);
+});
+
 
 console.log("Starting server on port " + port);
 server.listen(port);
