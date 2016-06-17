155b8ee76378d7fba3a3cbce4bae8fd2a923a048
Henry Xiang <hxxiang@gmail.com>
2016-06-17 15:20:06 -0700

    added material-ui library

--- src/main/javascript/app/component/TestApp.jsx
+++ src/main/javascript/app/component/TestApp.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import moment from 'moment';
+// import ReactDOM from 'react-dom';
+// import moment from 'moment';
 import fetech from 'isomorphic-fetch';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import FontIcon from 'material-ui/FontIcon';
+import {blue500, red500, greenA200} from 'material-ui/styles/colors';
+
+// Needed for onTouchTap or onClick event
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const TestApp = React.createClass({
 
@@ -13,15 +21,22 @@ const TestApp = React.createClass({
     const {users} = this.state;
     return (
       <div>
+
         <h3>User List</h3>
         <ul>
           {users.map((u,i) => (<li key={i}>{u.id}, {u.userName}, {u.firstName}, {u.lastName}, {u.birthday}</li>))}
         </ul>
+          <FontIcon
+              className="muidocs-icon-action-home"
+              style={iconStyles}
+              color={red500}
+              hoverColor={greenA200}
+          />
       </div>
     )
   },
 
-  componentDidMount() {
+  componentWillMount() {
       fetch('/users', {
           credentials: 'include'
       })
